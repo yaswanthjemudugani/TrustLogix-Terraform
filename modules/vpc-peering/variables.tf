7f8396eda1b433ce9894785980d3cfa0db57e69f
Yaswanth Jemudugani <yaswanth.professional@gmail.com>
2025-10-24 01:27:44 +0530

    Update variables.tf

--- modules/vpc-peering/variables.tf
+++ modules/vpc-peering/variables.tf
@@ -4,14 +4,20 @@ variable "vpc_id_cp" {
 variable "vpc_id_dp" {
 }
 
-variable "route_table_id_cp" {
-}
-
-variable "route_table_id_dp" {
-}
-
 variable "vpc_cidr_cp" {
 }
 
 variable "vpc_cidr_dp" {
+}
+
+variable "public_route_table_id_cp" {
+}
+
+variable "public_route_table_id_dp" {
+}
+
+variable "private_route_table_id_cp" {
+}
+
+variable "private_route_table_id_dp" {
 }
