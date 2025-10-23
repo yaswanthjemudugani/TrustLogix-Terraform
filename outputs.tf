5266381d485a4f64dfc5a92e3d6707dfcc1c7c3e
Yaswanth Jemudugani <yaswanth.professional@gmail.com>
2025-10-24 01:06:50 +0530

    Update outputs.tf
    Removed unused security group outputs and added vpc peering output.

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,10 @@ output "control_plane_vpc_id" {
   value = module.control_plane_vpc.vpc_id
 }
 
-output "control_plane_sg_id" {
-  value = module.control_plane_vpc.security_group_id
-}
-
 output "data_plane_vpc_id" {
   value = module.data_plane_vpc.vpc_id
 }
 
-output "data_plane_sg_id" {
-  value = module.data_plane_vpc.security_group_id
+output "vpc_peering_id" {
+  value = module.vpc_peering.vpc_peering_id
 }
