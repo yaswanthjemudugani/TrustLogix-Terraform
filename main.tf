8744bfb60b6a1a1f3c392f2553bb11b9d41fa8e4
Yaswanth Jemudugani <yaswanth.professional@gmail.com>
2025-10-24 00:39:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ module "control_plane_vpc" {
   vpc_name = "control-plane-vpc"
   vpc_cidr = "10.0.0.0/16"
   az       = "ap-south-1a"
+  admin_ip = "103.52.38.125/32"
 }
 
 module "data_plane_vpc" {
@@ -10,4 +11,5 @@ module "data_plane_vpc" {
   vpc_name = "data-plane-vpc"
   vpc_cidr = "192.168.0.0/16"
   az       = "ap-south-1b"
+  admin_ip = "103.52.38.125/32"
 }
