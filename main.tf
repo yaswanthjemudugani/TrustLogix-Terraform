b7e5fca8ab2430f1e8305b000413114a2a4ffe6b
Yaswanth Jemudugani <yaswanth.professional@gmail.com>
2025-10-24 01:06:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,3 +13,13 @@ module "data_plane_vpc" {
   az       = "ap-south-1b"
   admin_ip = "103.52.38.125/32"
 }
+
+module "vpc_peering" {
+  source            = "./modules/vpc-peering"
+  vpc_id_cp         = module.control_plane_vpc.vpc_id
+  vpc_id_dp         = module.data_plane_vpc.vpc_id
+  route_table_id_cp = module.control_plane_vpc.public_route_table_id
+  route_table_id_dp = module.data_plane_vpc.public_route_table_id
+  vpc_cidr_cp       = "10.0.0.0/16"
+  vpc_cidr_dp       = "192.168.0.0/16"
+}
