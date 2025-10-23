47c43677e4741b840f4ce9638adf4446a9e5d2ad
Yaswanth Jemudugani <yaswanth.professional@gmail.com>
2025-10-24 01:04:38 +0530

    Create variables.tf

---
+++ modules/vpc-peering/variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_id_cp" {
+}
+
+variable "vpc_id_dp" {
+}
+
+variable "route_table_id_cp" {
+}
+
+variable "route_table_id_dp" {
+}
+
+variable "vpc_cidr_cp" {
+}
+
+variable "vpc_cidr_dp" {
+}
