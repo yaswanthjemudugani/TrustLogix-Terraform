c00a05de041910690ed9e3d1c48d03765ab1eaa6
Yaswanth Jemudugani <yaswanth.professional@gmail.com>
2025-10-24 00:37:34 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,14 @@ output "control_plane_vpc_id" {
   value = module.control_plane_vpc.vpc_id
 }
 
+output "control_plane_sg_id" {
+  value = module.control_plane_vpc.security_group_id
+}
+
 output "data_plane_vpc_id" {
   value = module.data_plane_vpc.vpc_id
+}
+
+output "data_plane_sg_id" {
+  value = module.data_plane_vpc.security_group_id
 }
