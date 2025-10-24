2951cf0cbdc874ff9dc921a61685451d68a5bfbe
Yaswanth Jemudugani <yaswanth.professional@gmail.com>
2025-10-24 21:49:27 +0530

    Update outputs.tf

--- Task2-ecs-fargate-nginx/outputs.tf
+++ Task2-ecs-fargate-nginx/outputs.tf
@@ -1,29 +1,23 @@
-# ECS Cluster Name
 output "ecs_cluster_name" {
-  value       = aws_ecs_cluster.main.name
+  value = aws_ecs_cluster.main.name
 }
 
-# ECS Service Name
 output "ecs_service_name" {
-  value       = aws_ecs_service.nginx_service.name
+  value = aws_ecs_service.nginx_service.name
 }
 
-# ECS Task Definition ARN
 output "ecs_task_definition" {
-  value       = aws_ecs_task_definition.nginx_task.arn
+  value = aws_ecs_task_definition.nginx_task.arn
 }
 
-# Public Subnet IDs
 output "public_subnet_ids" {
-  value       = aws_subnet.public[*].id
+  value = aws_subnet.public[*].id
 }
 
-# ECS Security Group ID
 output "ecs_security_group_id" {
-  value       = aws_security_group.ecs_sg.id
+  value = aws_security_group.ecs_sg.id
 }
 
-# Info: Fargate public IP
 output "note_task_public_ip" {
-  value       = "Check ECS Console → Cluster → Service → Task → Public IP"
+  value = "Check ECS Console → Cluster → Service → Task → Public IP"
 }
